46f7f92a2deda124a1850f44545eed379a6e5e61
println <live.proto@hotmail.com>
2013-07-06 02:45:10 -0300

    tela de edicao

---
+++ src/com/socialbar/android/activities/EditBarActivity.java
@@ -0,0 +1,79 @@
+package com.socialbar.android.activities;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.socialbar.android.R;
+import com.socialbar.android.activities.advance.resources.GenericActivity;
+import com.socialbar.android.activities.advance.resources.GenericActivitySlider;
+
+public class EditBarActivity extends Activity implements OnClickListener {
+	private GenericActivity genericActivity;
+
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bar_edit);
+
+		/**
+		 * barra manipulação
+		 */
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		/**
+		 * efeitos da activity
+		 */
+
+		this.genericActivity = new GenericActivitySlider(this);
+		this.genericActivity.resume();
+
+		/**
+		 * Botoes
+		 */
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * evento voltar
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			this.onBackPressed();// encerra a activity
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public void onBackPressed() {
+		this.genericActivity.finish();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		MenuInflater inflater = getMenuInflater();
+		
+		inflater.inflate(R.menu.menu_bar_edit, menu);
+		return true;
+	}
+
+}
+
