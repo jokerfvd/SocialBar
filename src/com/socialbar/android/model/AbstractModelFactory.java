ef8b44434fa59ca2420df066bb1c9ec07bcf89f2
println <live.proto@hotmail.com>
2013-07-09 21:18:05 -0300

    inclusao do javadoc da api radar e model

--- src/com/socialbar/android/model/AbstractModelFactory.java
+++ src/com/socialbar/android/model/AbstractModelFactory.java
@@ -2,14 +2,21 @@ package com.socialbar.android.model;
 
 import com.socialbar.android.model.dummy.FactoryDummy;
 import com.socialbar.android.model.provider.FactoryProvider;
+
 /**
  * Class <code>fabrica de modelos</code>.
  */
 public class AbstractModelFactory {
-	public static Model getInstance(String... factory){
-		if(factory.length > 0 &&  factory[0].toUpperCase().equals("DUMMY"))
+	/**
+	 * Fabricar um modelo de acordo com a necessidade do usuario
+	 * 
+	 * @param factory
+	 * @return
+	 */
+	public static Model getInstance(String... factory) {
+		if (factory.length > 0 && factory[0].toUpperCase().equals("DUMMY"))
 			return FactoryDummy.getInstance();
 		return FactoryProvider.getInstance();
 	}
-	
+
 }
--- src/com/socialbar/android/model/Product.java
+++ src/com/socialbar/android/model/Product.java
@@ -4,20 +4,56 @@ package com.socialbar.android.model;
  * Interface <code>Product</code>.
  */
 public interface Product {
+	/**
+	 * Tipo de produto
+	 * 
+	 * @author Felipe
+	 * 
+	 */
 	enum Type {
 		// TODO: definir tipos de produtos
 	};
 
+	/**
+	 * pegar nome
+	 * 
+	 * @return
+	 */
 	String getName();
 
+	/**
+	 * pegar o preco do produto
+	 * 
+	 * @return
+	 */
 	double getPrice();
 
+	/**
+	 * pegar o tipo do produto
+	 * 
+	 * @return
+	 */
 	Product.Type getType();
 
+	/**
+	 * setar o nome do produto
+	 * 
+	 * @param name
+	 */
 	void setName(String name);
 
+	/**
+	 * setar o preco do produto
+	 * 
+	 * @param price
+	 */
 	void setPrice(double price);
 
-	void setType(Product.Type type);	
-	
+	/**
+	 * setar o tipo do produto
+	 * 
+	 * @param type
+	 */
+	void setType(Product.Type type);
+
 }
